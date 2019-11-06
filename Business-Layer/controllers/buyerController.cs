7d6b6bd00ab8b8b33435a8db11d60c967c6586d6
Daniel Grew <Dgrew0287@conestogac.on.ca>
2019-11-06 14:38:06 -0500

    Add project files.

---
+++ Business-Layer/controllers/buyerController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_ap_dg_js_sm
+{
+    class buyerController
+    {
+    }
+}
---
+++ configuration/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TMS_ap_dg_js_sm
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
