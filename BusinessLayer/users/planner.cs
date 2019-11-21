ef6443480e58bd2cc0ca090139f9d9208708433f
js-numberone <56318658+js-numberone@users.noreply.github.com>
2019-11-21 15:11:46 -0500

    Started Planner commneting and methods

--- BusinessLayer/users/planner.cs
+++ BusinessLayer/users/planner.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace TMS_ap_dg_js_sm
 {
+    /// \class Planner
+    /// <summary>
+    /// Represents planner user object fore the TMS system
+    /// </summary>
+    /// <exception cref="fileIoException">Exception during file reading or writing in the CheckTripStatus() function</exception>
     class Planner
     {
+        private Order workingOrder;
+        private bool pendingOrders;
+
+        public int SelectCarrier()
+        {
+            int done = 0;
+
+            return done;
+        }
     }
 }
