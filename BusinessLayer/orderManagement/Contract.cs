f18eeeb4dee053a1252da40afc8ccfff6ec122c5
js-numberone <56318658+js-numberone@users.noreply.github.com>
2019-11-21 21:42:01 -0500

    Contract class updated with comments

--- BusinessLayer/orderManagement/Contract.cs
+++ BusinessLayer/orderManagement/Contract.cs
@@ -3,30 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMS_ap_dg_js_sm.Business_Layer.users;
 
 namespace TMS_ap_dg_js_sm.Business_Layer.orderManagement
 {
+    /// \class Contract
+    /// <summary>
+    /// This class represents a Contract object for the TMS system
+    /// </summary>
+    /// <exception cref="fileIoException">Exception during file reading or writing in all functions</exception>
     class Contract
     {
-        private string cName;
-        private int jType;
-        private int vType;
-        private int Quant;
-        private string Orig;
-        private string Dest;
+        private string cName;///< Holds the carrier name as a string
+        private int jType;///< Holds the job type as a string
+        private int vType;///< Holds the van type as an integer
+        private int Quant;///< Holds the quantity of vans available
+        private string Orig;///< Holds origin city
+        private string Dest;///< Holds the destination city
 
 
-        public Contract(string NewContract)
+        /// <summary>
+        /// This method queries the Contract Marketplace to search for the desired contract 
+        /// </summary>
+        /// <returns>0 if successfully queried table. 1 if failed</returns>
+
+        public int GetContract()
         {
-            string[] words = NewContract.Split(',');
 
-            cName = words[0];
-            //Potentially implement try/catch block.
-            jType = Int32.Parse(words[1]);
-            vType = Int32.Parse(words[2]);
-            Quant = Int32.Parse(words[3]);
-            Orig = words[4];
-            Dest = words[5];
+            int done = 0;
+
+            try
+            {
+                //SELECT conrtact from contract marketplace
+                done = 1;//Set to 1 for testing
+            }
+            catch (Exception fileIoException)
+            {
+                Logger.Log(fileIoException.ToString());
+            }
+            return done;
+
         }
     }
 }
