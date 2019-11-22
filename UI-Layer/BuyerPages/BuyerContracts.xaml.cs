092871e89fa418c4fa1d825300c47a46771cb66f
Daniel Grew <Dgrew0287@conestogac.on.ca>
2019-11-22 13:20:50 -0500

    Created: BuyerContracts.xaml

---
+++ UI-Layer/BuyerPages/BuyerContracts.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TMS_ap_dg_js_sm
+{
+    /// <summary>
+    /// Interaction logic for BuyerContracts.xaml
+    /// </summary>
+    public partial class BuyerContracts : Page
+    {
+        public BuyerContracts()
+        {
+            InitializeComponent();
+        }
+    }
+}
