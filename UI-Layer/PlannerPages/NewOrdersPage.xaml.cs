276af63d3843c015f44a60c0a1f3aa6f934e7cf4
Daniel Grew <Dgrew0287@conestogac.on.ca>
2019-11-22 13:52:01 -0500

    Updated: NewOrdersPage.xaml, NewOrdersPage.xaml.cs

--- UI-Layer/PlannerPages/NewOrdersPage.xaml.cs
+++ UI-Layer/PlannerPages/NewOrdersPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace TMS_ap_dg_js_sm
 {
@@ -23,6 +24,49 @@ namespace TMS_ap_dg_js_sm
         public NewOrdersPage()
         {
             InitializeComponent();
+            ThreadStart ts = new ThreadStart(refreshTimer);
+            Thread ordersThread = new Thread(ts);
+            ordersThread.Start();
+        }
+
+        /// <summary>
+        /// The refresh timer is to be set as a thread that fires every 10 seconds. When it fires, it calls the refreshOrders() method.
+        /// This should automatically refresh the page every 10 seconds (or 10000 milliseconds)
+        /// </summary>
+        private void refreshTimer()
+        {
+            refreshOrders();
+            Thread.Sleep(10000);        // need to put hardcoded value into external source to be accessed
+        }
+
+        /// <summary>
+        /// This method calls an external method within the business logic layer, which will return a list of orders which will then be used
+        /// to populate the datagrid with the most up to date orders that are marked for completion
+        /// </summary>
+        public void refreshOrders()
+        {
+
+        }
+
+        /// <summary>
+        /// This event fires when the user selects something in the datagrid. It loads a local data member with the information displayed 
+        /// in the datagrid for use in generating an order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContractsGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// This is a button click event that calls on the refreshOrders() method, which will load the most current list of orders onto the screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            refreshOrders();            // call the refreshOrders method
         }
     }
 }
