a6410a2a39f7a31507cd6f487309da0d80f49f19
Sasha Malesevic <smalesevic7531@conestogac.on.ca>
2019-11-22 12:04:16 -0500

    Added comments for Buyer, LocalComm, ExternalComm

--- BusinessLayer/users/buyer.cs
+++ BusinessLayer/users/buyer.cs
@@ -7,34 +7,61 @@ using TMS_ap_dg_js_sm.Data_Access;
 
 namespace TMS_ap_dg_js_sm
 {
-    /// \class Admin
+    /// \class Buyer
     /// <summary>
     /// Represents a buyer user of the TMS program
     /// </summary>
     class Buyer
     {
         /// <summary>
-        /// This method will allow the buyer user to fetch contracts from the Contract Marketplace database.
-        /// These values will be updated in the App.config file.
+        /// This method will allow the Buyer to get contracts from the Contract Marketplace database. 
         /// </summary>
-        /// <param name="newIP">The requested new IP address</param>
-        /// <param name="newPort">The requested new port number - default to blank in the case where the user only
-        ///                       wants to update the ip address.</param>
-        /// <returns>Int representing the successful change in IP/Port values.</returns>
-        /// <exception cref="fileIoException">Exception during file IO</exception>
+        /// <returns>List<Contract> of the contracts from the Contract Marketplace.</returns>
         public List<Contract> GetContracts()
         {
             List<Contract> contractList = new ExternalComm().GetContracts();
             return contractList; 
         }
 
-        // the searchItem would be the contractID
+        /// <summary>
+        /// This method will allow the Buyer to create a new Order that will be added to
+        /// the local database. 
+        /// <param name="searchItem">The identifier for the Contract that will be used 
+        ///                         to create the new Order</param>
+        /// </summary>
+        /// <returns>Order that has been created.</returns>
         public Order CreateOrder(string searchItem)
         {
             Order newOrder = new LocalComm().CreateOrder(searchItem);
             return newOrder;
         }
 
+        /// <summary>
+        /// This method will allow the Buyer to view Orders marked by completion by the Planner.
+        /// </summary>
+        /// <returns>List<Order></Order> of the completed Orders.</returns>
+        public List<Order> GetCompletedOrders()
+        {
+            List<Order> completedOrderList = new LocalComm().GetCompletedOrders();
+            return completedOrderList;
+        }
+
+        /// <summary>
+        /// This method will allow the Buyer to generate an invoice as a text file for the 
+        /// requested completedOrder.
+        /// </summary>
+        /// /// <param name="searchItem">The identifier for the Order that requires
+        ///                             an invoice</param>
+        /// <returns>Int representing invoice was successfully generated.</returns>
+        public int GenerateInvoice(string searchItem)
+        {
+            int done = 0;
+
+            done = 1; //set to true for testing
+            string invoiceInformation = new LocalComm().GetInvoice(searchItem);
+
+            return done;
+        }
 
     }
 }
--- Data-Access/LocalComm.cs
+++ Data-Access/LocalComm.cs
@@ -6,11 +6,44 @@ using System.Threading.Tasks;
 
 namespace TMS_ap_dg_js_sm
 {
+    /// \class LocalComm
+    /// <summary>
+    /// Responsible for any communications with the local TMS database.
+    /// </summary>
     class LocalComm
     {
+        /// <summary>
+        /// This method adds a new Order to the TMS database.
+        /// <param name="searchItem">The identifier for the Contract that will be used 
+        ///                         to create the new Order</param>
+        /// </summary>
+        /// <returns>Order that has been created.</returns>
         public Order CreateOrder(string searchItem)
         {
+            Order newOrder = null;
             return newOrder;
+        }
+
+        /// <summary>
+        /// This method queries the TMS database for completed Orders.
+        /// </summary>
+        /// <returns>List<Order></Order> of the completed Orders.</returns>
+        public List<Order> GetCompletedOrders()
+        {
+            List<Order> completedOrderList = null;
+            return completedOrderList;
+        }
+
+        /// <summary>
+        /// This method queries the TMS database for the invoice requested.
+        /// </summary>
+        /// <param name="searchItem">The identifier for the Order that requires
+        ///                             an invoice</param>
+        /// <returns>String containing the information required for an invoice.</returns>
+        public string GetInvoice(string searchItem)
+        {
+            string invoice = "";
+            return invoice;
         }
     }
 }
