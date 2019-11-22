3b5b24a07f151a806dc9287dc891b118f7a0f15d
js-numberone <56318658+js-numberone@users.noreply.github.com>
2019-11-22 11:56:49 -0500

    Controller commenting and axception updated

--- Business-Layer/controllers/controller.cs
+++ Business-Layer/controllers/controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMS_ap_dg_js_sm.Business_Layer.users;
 
 namespace TMS_ap_dg_js_sm
 {
@@ -13,27 +14,7 @@ namespace TMS_ap_dg_js_sm
     /// </summary>
     class Controller
     {
-        private bool isConnected; /// Checks whether the user is connected to the database
-       
-
-        public bool IsConnected     // mutator and accessor for isConnectd
-        {
-            get { return isConnected; }
-            set { isConnected = value; }
-        }
-
-        /// <summary>
-        /// This method establishes a connectin to the database required by the current user
-        /// </summary>
-        /// <param name="currentIP">The requested IP address</param>
-        /// <param name="currentPort">The requested PORT number</param>
-        /// <returns>0 if no connection is established and 1 if connection exists</returns>
-        public int ConnectToDB(string currentIP, string currentPort)
-        {
-
-            return 0;
-        }
-
+        
         /// <summary>
         /// This method queries the orders table for all active orders
         /// </summary>
@@ -41,7 +22,19 @@ namespace TMS_ap_dg_js_sm
         
         public int ShowActiveOrders()
         {
-            return 0;
+            int done = 0;
+
+            try
+            {
+                done = 1;
+                //Try and connect to DB
+            }
+            catch(Exception e)
+            {
+                Logger.Log("Failed", 1);
+            }
+
+            return done;
         }
 
         /// <summary>
@@ -50,80 +43,123 @@ namespace TMS_ap_dg_js_sm
         /// <returns>0 if the query was successful and 1 is qeury failed.</returns>
         public int ShowCompletedOrders()
         {
-            return 0;
+            int done = 0;
+
+            try
+            {
+                done = 1;
+                //Try and connect to DB
+            }
+            catch (Exception e)
+            {
+                Logger.Log("Failed", 1);
+            }
+
+            return done;
         }
 
         /// <summary>
         /// This method enables the Admin user to be able to change the target IP addess
         /// </summary>
         /// <param name="newIP">Holds the new IP address from the admin user</param>
-        public void UpdateIP(string newIP)
+        public int UpdateIP(string newIP)
         {
-            
+            int done = 0;
+
+            try
+            {
+                done = 1;
+                //Try and connect to DB
+            }
+            catch (Exception e)
+            {
+                Logger.Log("Failed", 1);
+            }
+
+            return done;
         }
 
         /// <summary>
         /// This method enables the Admin user to be able to change the target IP addess
         /// </summary>
         /// <param name="newPort">Holds the new PORT number from the admin user</param>
-        public void UpdatePort(string newPort)
+        public int UpdatePort(string newPort)
         {
+            int done = 0;
 
+            try
+            {
+                done = 1;
+                //Try and connect to DB
+            }
+            catch (Exception e)
+            {
+                Logger.Log("Failed", 1);
+            }
+
+            return done;
         }
 
-        /*
-         * Function :
-         * Parameters :
-         * Description : 
-         * Returns : 
-         */
-        public void UpdateCarrier()
+        /// <summary>
+        /// This method updates the selected carrier's information
+        /// </summary>
+        /// <param name="carrierID">Holds the carrier ID of the selected carrier</param>
+        /// <returns>0 if successfully updated, 1 if update fails.</returns>
+        
+        public int UpdateCarrier(int carrierID)
         {
-
+            int done = 0;
+            try
+            {
+                done = 1;
+            }
+            catch(Exception e)
+            {
+                Logger.Log("Failed", 1);
+            }
+            return done;
         }
 
-        /*
-         * Function :
-         * Parameters :
-         * Description : 
-         * Returns : 
-         */
-        public void UpdateRateTable()
-        {
 
+        /// <summary>
+        /// This method allows the Admin to update the van rates table
+        /// </summary>
+        /// <param name="rateID">Holds the rateID for the selected vant rate which needs to be updated</param>
+        /// <returns>0 if successfully updated, 1 if update fails.</returns>
+        public int UpdateRateTable(string rateID)
+        {
+            int done = 0;
+
+            try
+            {
+                done = 1;
+            }
+            catch(Exception e)
+            {
+                Logger.Log("Failed", 1);
+            }
+            return done;
         }
 
-        /*
-         * Function :
-         * Parameters :
-         * Description : 
-         * Returns : 
-         */
-        public void UpdateRoutTable()
+        /// <summary>
+        /// This method allows the admin to update the route table
+        /// </summary>
+        /// <param name="routeID">Holds the routeID of the route which needs to be updated</param>
+        /// <returns>0 if successfully updated, 1 if update fails.</returns>
+        public int UpdateRoutTable(int routeID)
         {
+            int done = 0;
 
-        }
+            try
+            {
+                done = 1;
+            }
+            catch(Exception e)
+            {
+                Logger.Log("Failed", 1);
+            }
 
-        /*
-         * Function :
-         * Parameters :
-         * Description : 
-         * Returns : 
-         */
-        public void ValidateLogin()
-        {
-
-        }
-
-        /*
-         * Function :
-         * Parameters :
-         * Description : 
-         * Returns : 
-         */
-        public void SendAnswer()
-        {
-
+            return done;
         }
     }
 }
