3da12a9097732d0052a116fac4b26a6d7909f5e2
Sasha Malesevic <smalesevic7531@conestogac.on.ca>
2019-12-09 14:49:00 -0500

    Fixed buyer planner contract

--- UI-Layer/BuyerPages/BuyerPage.xaml.cs
+++ UI-Layer/BuyerPages/BuyerPage.xaml.cs
@@ -158,8 +158,16 @@ namespace TMS
         {
             try
             {
-                // call function that will fill the datagrid
-                fillList();
+                try
+                {
+                    ContractGrid.Items.Clear();
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log("Contract datagrid is already cleared\n" + ex);
+                }
+                    // call function that will fill the datagrid
+                    fillList();
                 // create thread that automatically refreshes datagrid for new contracts from the marketplace
                 fillGrid();
             }
