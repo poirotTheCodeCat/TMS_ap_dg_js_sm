992cb53aab24aaf645f4a0d09bd413c251912fa4
Daniel Grew <Dgrew0287@conestogac.on.ca>
2019-12-09 13:05:17 -0500

    Update UI

--- UI-Layer/BuyerPages/BuyerPage.xaml.cs
+++ UI-Layer/BuyerPages/BuyerPage.xaml.cs
@@ -146,15 +146,7 @@ namespace TMS
 
         private void generateInvoice(Contract contract)
         {
-            string invoiceString = "Contract ID: " + contract.ContractID.ToString() + "\n"
-                + "Customer: " + contract.ClientName + "\n"
-                + "Job Type: " + contract.JobType.ToString() + "\n"
-                + "Van Type " + contract.VanType.ToString() + "\n"
-                + "Quantity " + contract.Quantity.ToString() + "\n"
-                + "Price: " + contract.Price.ToString();
-
-            MessageBox.Show(invoiceString);
-
+            buyer.GenerateInvoice(contract);
         }
 
         /// <summary>
